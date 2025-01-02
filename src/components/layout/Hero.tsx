cb9c924eab30a5f43c68954299eed7c7e523310b
cangcode <cangcodes@gmail.com>
2025-01-03 00:06:06 +0800

    escaped character bug

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = async () => {
   return (
     <div className='flex flex-col justify-center md:items-center md:text-center px-4 gap-2 mt-10'>
         <h1 className='font-bold text-primGreen text-4xl md:text-7xl '>Halo{!session && '!'} <span className='uppercase text-secGreen'>{session && session?.user.name + '!'}</span> welcome to <span className='text-primGreen font-bold'>Nutri Ta'</span></h1>
-        <p className='md:text-2xl md:w-[35rem]'>Aplikasi pakar gizi dengan akses cepat dan mudah untuk mengelola kebutuhan <span className='text-primGreen font-bold'>Nutri</span>si <span className='text-primGreen font-bold'>ta'</span> sehari-hari.</p>
+        <p className='md:text-2xl md:w-[35rem]'>Aplikasi pakar gizi dengan akses cepat dan mudah untuk mengelola kebutuhan <span className='text-primGreen font-bold'>Nutri</span>si <span className='text-primGreen font-bold'>ta&apos;</span> sehari-hari.</p>
         <Button text='Pammulai mi' className={cn('mt-3 md:mx-auto')}/>
     </div>
   )
