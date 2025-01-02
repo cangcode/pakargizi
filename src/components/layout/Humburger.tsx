1a2d98e399a3a5a6e85d1d96aadd903ef4343841
cangcode <cangcodes@gmail.com>
2025-01-02 14:39:18 +0800

    fix humburger bug

--- src/components/layout/Humburger.tsx
+++ src/components/layout/Humburger.tsx
@@ -4,37 +4,44 @@ import { useEffect, useState } from 'react';
 import { MenuList } from '../ui/MenuList';
 
 export const Humburger = () => {
-  const [toggleNav,setToggleNav] = useState(false)
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [toggleNav, setToggleNav] = useState(false);
+  const [windowWidth, setWindowWidth] = useState<number | undefined>(undefined); // Awalnya undefined
 
-
+  // Menunda akses `window` sampai komponen dirender di klien
   useEffect(() => {
-    const handleResize = () => {
+    // Mengecek apakah berada di sisi klien
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+      window.addEventListener('resize', handleResize);
+
+      // Set windowWidth saat pertama kali di-load
       setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
   }, []);
 
   return (
-    <div onClick={()=>setToggleNav(!toggleNav)}>
+    <div onClick={() => setToggleNav(!toggleNav)}>
       <div className='md:hidden relative'>
-      {toggleNav ? (
-        <>
-          <X size={38}/>
-          <div className='absolute bottom-[-10rem] right-2'>
-            <MenuList/>
-          </div>
-        </>
-      ):(
-        <Menu size={38}/>
-      )}
+        {toggleNav ? (
+          <>
+            <X size={38} />
+            <div className='absolute bottom-[-10rem] right-2'>
+              <MenuList />
+            </div>
+          </>
+        ) : (
+          <Menu size={38} />
+        )}
       </div>
-      {windowWidth > 768 ? (
-          <MenuList/>
-      ):null}
+      {windowWidth && windowWidth > 768 ? ( // Periksa apakah windowWidth sudah terisi
+        <MenuList />
+      ) : null}
     </div>
-  )
-}
+  );
+};
