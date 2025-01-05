b462a775e76e4eeed9d0fac82b9eabda60324691
cangcode <cangcodes@gmail.com>
2025-01-05 16:59:08 +0800

    feat: improving rendering speed of calorie calculator

--- src/app/kalkulator-kalori/page.tsx
+++ src/app/kalkulator-kalori/page.tsx
@@ -1,12 +1,25 @@
-import { auth } from '@/auth'
+'use client'
 import FormHitungKalori from '@/components/layout/FormHitungKalori'
 import React from 'react'
+import { useSession } from "next-auth/react"
 
-const page = async () => {
-    const session = await auth()
-    if (!session?.user) return <div className='h-96 font-semibold text-primGreen justify-center flex items-center'>Login terlebih dahulu!</div>
-  return (
-    
+
+const page = () => {
+  const { data: session, status } = useSession()
+  if (status === 'loading') {
+    return (
+      <div className='w-full h-screen flex justify-center items-center'>
+        <div className='w-20 h-20 animate-pulse flex text-xl font-semibold text-secGreen'> Loading.. </div>
+      </div>
+    )
+  }
+    if (!session?.user) {
+      return  (
+        <div className='h-96 font-semibold text-primGreen justify-center flex items-center'>Login terlebih dahulu!</div>
+      )
+
+    }
+    return (
     <div>
         <FormHitungKalori/>
     </div>
